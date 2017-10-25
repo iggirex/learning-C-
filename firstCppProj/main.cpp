4dd998e34ce42c234a760fdbc18d1add80dbe8c1
unknown <iggirex@gmail.com>
2017-10-24 21:27:40 -0600

    classes in diff files

--- firstCppProj/main.cpp
+++ firstCppProj/main.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
-
+#include "Burrito.h"
 using namespace std;
-
-class IggisClass{
-    public:
-        IggisClass(string z){
-            setName(z);
-        };
-        void setName(string x){
-            name = x;
-        }
-        string getName(){
-            return name;
-        }
-    private:
-        string name = "Ignacius";
-};
 
 int main()
 {
-    IggisClass iggio("Lucky Iggi\n");
-    cout << iggio.getName();
-
-    IggisClass iggiClone("You are me");
-    cout << iggiClone.getName();
+    Burrito bo;
     return 0;
 }
 
