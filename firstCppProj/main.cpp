22c36fcd7298e338c7c49f31ba1e567b01d58578
unknown <iggirex@gmail.com>
2017-10-24 20:47:04 -0600

    getter and setter

--- firstCppProj/main.cpp
+++ firstCppProj/main.cpp
@@ -2,68 +2,23 @@
 
 using namespace std;
 
-int multiply(int a, int b){
- return 4 * 5;
-}
-
-int add(int a, int b){
-    return a + b;
-}
-
-int divide(int a, int b){
-    return a / b;
-}
-
-int subtract(int a, int b){
-    return a - b;
-}
+class IggisClass{
+    public:
+        void setName(string x){
+            name = x;
+        }
+        string getName(){
+            return name;
+        }
+    private:
+        string name = "Ignacius";
+};
 
 int main()
 {
-    string choice;
-    int a;
-    int b;
-
-    cout << "What would you like to calculate?";
-    cin >> choice;
-
-    if(choice == "add"){
-        cout << "First number to add: ";
-        cin >> a;
-
-        cout << "Second number to add: ";
-        cin >> b;
-
-        cout << "The sum is: ";
-        cout << add(a, b);
-    } else if(choice == "multiply"){
-        cout << "First number to multiply: ";
-        cin >> a;
-
-        cout << "Second number to multiply: ";
-        cin >> b;
-
-        cout << "The result is: ";
-        cout << multiply(a, b);
-    } else if(choice == "divide"){
-        cout << "First number to divide: ";
-        cin >> a;
-
-        cout << "Second number to divide: ";
-        cin >> b;
-
-        cout << "The result is: ";
-        cout << divide(a, b);
-    } else if(choice == "subtract"){
-        cout << "Subtract: ";
-        cin >> b;
-
-        cout << "from: ";
-        cin >> a;
-
-        cout << "The result is: ";
-        cout << subtract(a, b);
-    }
+    IggisClass iggio;
+    iggio.setName("Sir Ig$ter");
+    cout << iggio.getName();
     return 0;
 }
 
